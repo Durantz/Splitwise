a555a14ebb3807e73c11f1a44e766a0e61351ca9
durantz <54364162+Durantz@users.noreply.github.com>
2026-03-12 19:16:21 +0000

    fix in import page

--- app/(app)/budget/import/page.tsx
+++ app/(app)/budget/import/page.tsx
@@ -1,9 +1,13 @@
 import { getMappings } from "@/app/(app)/budget/server";
 import { ImportWizard } from "./ImportWizard";
+import { getCategoryRules } from "../edit-categories/server";
 
 export const metadata = { title: "Importa estratto conto" };
 
 export default async function ImportPage() {
-  const existingMappings = await getMappings();
-  return <ImportWizard existingMappings={existingMappings} />;
+  const [existingMappings, rules] = await Promise.all([
+    getMappings(),
+    getCategoryRules(),
+  ]);
+  return <ImportWizard existingMappings={existingMappings} rules={rules} />;
 }
